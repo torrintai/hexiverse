c6237b898af7d5ca0b21590b83313721163a6c00
torrintai <torrintaivvi@protonmail.com>
2022-08-16 14:11:46 -0500

    nav change

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -6,11 +6,11 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}><Link to='/quotes' style={{textDecoration:'none', color: 'white'}}>HEX Verse</Link></div>
+      <div className={classes.logo}><Link to='/home' style={{textDecoration:'none', color: 'white'}}>HEX Verse</Link></div>
       <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to='/quotes' activeClassName={classes.active}>Dashboard</NavLink> 
+            <NavLink to='/home' activeClassName={classes.active}>Dashboard</NavLink> 
           </li>
           <li><NavLink to='/new-quote' activeClassName={classes.active}>Add Quotes</NavLink> 
           </li>
